68cb3561939970d0f6abf8d5af913a05d0e37dba
Mike Marsello <mmarsello@austin.utexas.edu>
2017-02-17 23:59:34 -0600

    initial commit

---
+++ themes/bootstrap/protest/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Extends methods from core/misc/form.js.
+ */
+
+(function ($, window, Drupal, drupalSettings) {
+
+  /**
+   * Behavior for "forms_has_error_value_toggle" theme setting.
+   */
+  Drupal.behaviors.customBehavior = {
+    attach: function (context) {
+
+    }
+  };
+
+
+})(jQuery, this, Drupal, drupalSettings);
